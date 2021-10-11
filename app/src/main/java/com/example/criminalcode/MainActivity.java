4280ca88fc154d84348584ca844607474f6cd1df
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-11 23:52:36 +0300

    add old toolbar

--- app/src/main/java/com/example/criminalcode/MainActivity.java
+++ app/src/main/java/com/example/criminalcode/MainActivity.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Objects;
+
 
 public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Уголовный кодекс 2021");
     }
 }
