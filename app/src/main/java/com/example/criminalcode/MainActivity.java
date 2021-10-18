f2fb5e8304b125b961ab56fd40bad3f8af6e1001
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-19 01:22:14 +0300

    add setOnClickListener for profileButton

--- app/src/main/java/com/example/criminalcode/MainActivity.java
+++ app/src/main/java/com/example/criminalcode/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.criminalcode;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(getSupportActionBar()).setTitle("Уголовный кодекс 2021");
+        ImageButton profileButton = (ImageButton) findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,profileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
