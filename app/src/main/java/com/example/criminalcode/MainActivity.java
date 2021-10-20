db43dbd051497e958a03308b83fdf4669c9bd423
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-21 01:30:45 +0300

    add readbutton

--- app/src/main/java/com/example/criminalcode/MainActivity.java
+++ app/src/main/java/com/example/criminalcode/MainActivity.java
@@ -3,28 +3,40 @@ package com.example.criminalcode;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.Objects;
-
+/*
+Уголовный кодекс тестовые задания -приложение которое поможет вам узнать лучше законадательные акты Российской Федерации
+21.10.2021  01:09
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Уголовный кодекс 2021");
-        ImageButton profileButton = (ImageButton) findViewById(R.id.profileButton);
+        getSupportActionBar().setTitle("Уголовный кодекс 2021");    //название
+        ImageButton profileButton = (ImageButton) findViewById(R.id.profileButton); //кнопка профиля
         profileButton.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,profileActivity.class);
-                startActivity(intent);
+                startActivity(intent);//переход на экран профиля
             }
         });
+        ImageButton readButton = (ImageButton) findViewById(R.id.readButton);   //кнопка чтения
+        readButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,readActivity.class);
+                startActivity(intent);  //переход на экран со статьями уголовного кодекса
+            }
+        });
+        //тут должны быть ещё две кнопки
+        //тестовые задания
+        //ошибки(мой прогресс)
     }
 }
