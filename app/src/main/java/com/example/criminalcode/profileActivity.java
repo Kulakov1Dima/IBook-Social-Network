56000a41beedff219f16deaf4568d7b8a43427f0
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-21 01:33:12 +0300

    update

--- app/src/main/java/com/example/criminalcode/profileActivity.java
+++ app/src/main/java/com/example/criminalcode/profileActivity.java
@@ -3,12 +3,24 @@ package com.example.criminalcode;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+/*
+Экран профиля показаны активность(в минутах), рекомендации и полезные ссылки
+Во врианте закрытого софта: сведения о тарифе pro версии (но наврятли так будет :) )
+ */
 public class profileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("Привет user!");    //название
+        //кнопка вернуться на главную
+        //посещаемость
+        //решено задач
+        //мои цели
+        //последняя страничка в теории (возращает пользователя на страничку чтения где он был последний раз)
+        //рекомендации
+        //отключить уведомления
+        //преобрести pro версию
     }
 }
