87aa1e11fc78bcefb370a087613c5cf059244628
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-25 23:19:36 +0300

    Настроена кнопка возврата на главную страницу

--- app/src/main/java/com/example/criminalcode/readActivity.java
+++ app/src/main/java/com/example/criminalcode/readActivity.java
@@ -2,6 +2,9 @@ package com.example.criminalcode;
 
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 //экран чтения-теоретический материал
 // по моим предположениям в приложении должны открываться PDF файлы
@@ -12,5 +15,18 @@ public class readActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
         //глава I и тд.
+        ActionBar actionBar = getSupportActionBar();        //получение доступа к ActionBar
+        actionBar.setHomeButtonEnabled(true);               //включение кнопки назад
+        actionBar.setDisplayHomeAsUpEnabled(true);          //отображение кнопки назад
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
