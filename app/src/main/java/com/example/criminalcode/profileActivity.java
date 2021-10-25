d2eccca4c8cc59c3ddd48ef1d7ca5ea9591f14f1
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-10-25 23:18:56 +0300

    Настроена кнопка возврата на главную страницу

--- app/src/main/java/com/example/criminalcode/profileActivity.java
+++ app/src/main/java/com/example/criminalcode/profileActivity.java
@@ -1,8 +1,11 @@
 package com.example.criminalcode;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 /*
 Экран профиля показаны активность(в минутах), рекомендации и полезные ссылки
 Во врианте закрытого софта: сведения о тарифе pro версии (но наврятли так будет :) )
@@ -13,8 +16,11 @@ public class profileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-        getSupportActionBar().setTitle("Привет user!");    //название
-        //кнопка вернуться на главную
+        ActionBar actionBar = getSupportActionBar();        //получение доступа к ActionBar
+        actionBar.setHomeButtonEnabled(true);               //включение кнопки назад
+        actionBar.setDisplayHomeAsUpEnabled(true);          //отображение кнопки назад
+        getSupportActionBar().setTitle("Добро пожаловать user!");     //название страницы
+
         //посещаемость
         //решено задач
         //мои цели
@@ -22,5 +28,14 @@ public class profileActivity extends AppCompatActivity {
         //рекомендации
         //отключить уведомления
         //преобрести pro версию
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
