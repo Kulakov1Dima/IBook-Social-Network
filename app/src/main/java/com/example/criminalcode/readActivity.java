0a02101818f63bcfb4c27c19e4477ed3083cf2cb
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-12 22:57:53 +0300

    Update readActivity.java

--- app/src/main/java/com/example/criminalcode/readActivity.java
+++ app/src/main/java/com/example/criminalcode/readActivity.java
@@ -25,13 +25,124 @@ public class readActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);          //отображение кнопки назад
 
         Button chapter1 = (Button) findViewById(R.id.chapter1);
-        chapter1.setOnClickListener(new View.OnClickListener(){//временно висит pageRead на первой кнопке, чтобы понять как вообще делать
+        chapter1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",0);
                 startActivity(intent);                      //переход на экран чтения статей(pdf файл)
             }
         });
+        Button chapter2 = (Button) findViewById(R.id.chapter2);
+        chapter2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",2);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter3 = (Button) findViewById(R.id.chapter3);
+        chapter3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",11);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter4 = (Button) findViewById(R.id.chapter4);
+        chapter4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",28);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter5 = (Button) findViewById(R.id.chapter5);
+        chapter5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",36);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter6 = (Button) findViewById(R.id.chapter6);
+        chapter6.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",39);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter7 = (Button) findViewById(R.id.chapter7);
+        chapter7.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",45);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter8 = (Button) findViewById(R.id.chapter8);
+        chapter8.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",76);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter9 = (Button) findViewById(R.id.chapter9);
+        chapter9.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",131);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter10 = (Button) findViewById(R.id.chapter10);
+        chapter10.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",192);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter11 = (Button) findViewById(R.id.chapter11);
+        chapter11.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",227);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
+        Button chapter12 = (Button) findViewById(R.id.chapter12);
+        chapter12.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(readActivity.this,pageRead.class);
+                intent.putExtra("Npage",233);
+                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        });
+
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
