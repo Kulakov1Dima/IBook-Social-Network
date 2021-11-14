d6d2926ba3c2b50f20ebb6a078f2d993e780dbb0
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-15 00:10:58 +0300

    Update pageRead.java

--- app/src/main/java/com/example/criminalcode/pageRead.java
+++ app/src/main/java/com/example/criminalcode/pageRead.java
@@ -1,23 +1,30 @@
 package com.example.criminalcode;
-
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
 
-import java.io.File;
 
 public class pageRead extends AppCompatActivity implements OnPageChangeListener {
+
+    private static SharedPreferences pageL;
+    private static SharedPreferences.Editor editor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_read);
+
+        pageL = getSharedPreferences("NpageL", Context.MODE_PRIVATE);
+        editor = pageL.edit();
 
         int nPage=0;
         nPage= getIntent().getIntExtra("Npage",0);
@@ -37,7 +44,12 @@ public class pageRead extends AppCompatActivity implements OnPageChangeListener 
 
     @Override
     public void onPageChanged(int page, int pageCount) {
-        setTitle(String.format("%s %s / %s","Уголовный кодекс: ", page + 1, pageCount));
+        setTitle(String.format("Уголовный кодекс"));
+        TextView pageN = findViewById(R.id.textView11);
+        pageN.setText((int)(page + 1)+"/"+ pageCount);
+
+        editor.putInt("NpageL",page).apply();
+        editor.putInt("pageC",pageCount).apply();
     }
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
