8490eb93b93a264ed704e55cb5dc1f678f48e9f7
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-15 10:02:38 +0300

    add DialogFragment

---
+++ app/src/main/java/com/example/criminalcode/CustomDialogFragment.java
@@ -0,0 +1,39 @@
+package com.example.criminalcode;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Process;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.annotation.NonNull;
+
+public class CustomDialogFragment extends DialogFragment {
+
+
+    @NonNull
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+
+
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        return builder
+                .setTitle("Продолжить с последней страницы?")
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setNegativeButton("нет", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                    }})
+                .setPositiveButton("да", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    Intent intent = new Intent(getActivity(),pageRead.class);
+                    intent.putExtra("Npage",readActivity.lastP);
+                    startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+            }
+        }).create();
+
+    }
+}
