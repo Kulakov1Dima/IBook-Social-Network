d85641897a45a56f59bafc957a15208693361199
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-15 10:11:53 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/criminalcode/MainActivity.java
+++ app/src/main/java/com/example/criminalcode/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.criminalcode;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
