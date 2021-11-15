c84f6b9f4fd97306872056281cd458e482598e3e
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-15 10:11:47 +0300

    Update CustomDialogFragment.java

--- app/src/main/java/com/example/criminalcode/CustomDialogFragment.java
+++ app/src/main/java/com/example/criminalcode/CustomDialogFragment.java
@@ -2,12 +2,9 @@ package com.example.criminalcode;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Process;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.annotation.NonNull;
