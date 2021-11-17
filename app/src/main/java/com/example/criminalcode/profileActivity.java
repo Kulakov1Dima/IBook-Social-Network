868f70106e0d2b89301d1ec1dda5db1b2e34336a
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-11-18 01:35:00 +0300

    code quality changes

--- app/src/main/java/com/example/criminalcode/profileActivity.java
+++ app/src/main/java/com/example/criminalcode/profileActivity.java
@@ -1,77 +1,67 @@
 package com.example.criminalcode;
-
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 /*
-Экран профиля показаны активность(в минутах), рекомендации и полезные ссылки
+Экран профиля показаны активность (в минутах), рекомендации и полезные ссылки
 Во варианте закрытого софта: сведения о тарифе pro версии (но наврятли так будет :) )
  */
 public class profileActivity extends AppCompatActivity {
-
-    private static SharedPreferences pageL;
-
-
+    SharedPreferences pageL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        pageL = getSharedPreferences("NpageL", Context.MODE_PRIVATE);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        pageL = getSharedPreferences("NpageL", Context.MODE_PRIVATE);
+        customizationActionBar();
+        textRead();                                         //функция прогрессбара и вывода номера последней страницы в профиле
+        buttons();
+    }
+
+    void customizationActionBar(){
         ActionBar actionBar = getSupportActionBar();        //получение доступа к ActionBar
+        assert actionBar != null;
         actionBar.setHomeButtonEnabled(true);               //включение кнопки назад
         actionBar.setDisplayHomeAsUpEnabled(true);          //отображение кнопки назад
         getSupportActionBar().setTitle("Добро пожаловать user!");     //название страницы
-
-        textRead();                                         //функция прогрессбара и вывода номера последней страницы в профиле
-
-        Button lastPage = (Button) findViewById(R.id.button2);
-        lastPage.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(profileActivity.this,pageRead.class);
-                intent.putExtra("Npage",pageL.getInt("NpageL",0));
-                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
-            }
-        });
     }
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
+    void buttons(){
+        Button lastPage = findViewById(R.id.button2);
+        lastPage.setOnClickListener(v -> {
+            Intent intent = new Intent(profileActivity.this,pageRead.class);
+            intent.putExtra("Npage",pageL.getInt("NpageL",0));
+            startActivity(intent);                      //переход на экран чтения статей(pdf файл)
+        });
     }
     void textRead(){                                        //функция прогрессбара и вывода номера последней страницы в профиле
         TextView pageN = findViewById(R.id.textView3);      //страницы в прогрессе чтения
-        pageN.setText((int)(pageL.getInt("NpageL",0) + 1)+"/"+ pageL.getInt("pageC",0));
+        String pageСounter = (pageL.getInt("NpageL",0) + 1) +"/"+ pageL.getInt("pageC",0);
+        pageN.setText(pageСounter);
 
         double maxProgress=pageL.getInt("pageC",0)/100; //переменная равная 1% от всех страниц
 
-        ProgressBar readPage =(ProgressBar)findViewById(R.id.progressBar2);
+        ProgressBar readPage = findViewById(R.id.progressBar2);
         readPage.setProgress((int)((pageL.getInt("NpageL",0) + 1)/maxProgress)); //процент прочитанного
 
-        readPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(profileActivity.this,pageRead.class);
-                intent.putExtra("Npage",pageL.getInt("NpageL",0));
-                startActivity(intent);                      //переход на экран чтения статей(pdf файл)
-            }
+        readPage.setOnClickListener(v -> {
+            Intent intent = new Intent(profileActivity.this,pageRead.class);
+            intent.putExtra("Npage",pageL.getInt("NpageL",0));
+            startActivity(intent);                      //переход на экран чтения статей(pdf файл)
         });
 
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            this.finish();
+            return true;
+        }
+        else return super.onOptionsItemSelected(item);
     }
 }
