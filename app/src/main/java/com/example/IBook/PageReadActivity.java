49f3f2e9cbb233264165062682b4b038a7e228ad
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-27 02:58:47 +0300

    Update PageReadActivity.java

--- app/src/main/java/com/example/IBook/PageReadActivity.java
+++ app/src/main/java/com/example/IBook/PageReadActivity.java
@@ -10,6 +10,7 @@ public class PageReadActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_read);
+        getSupportActionBar().setTitle("Сообщения");                                                  //название страницы
 
     }
 }
