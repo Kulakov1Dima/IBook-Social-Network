9da5757458c51e5f19c23e88572a15a6c97b9263
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-27 02:58:43 +0300

    Update ProfileActivity.java

--- app/src/main/java/com/example/IBook/ProfileActivity.java
+++ app/src/main/java/com/example/IBook/ProfileActivity.java
@@ -1,33 +1,29 @@
 package com.example.IBook;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-/*
-Экран профиля показаны активность (в минутах), рекомендации и полезные ссылки
-Во варианте закрытого софта: сведения о тарифе pro версии (но наврятли так будет :) )
- */
+
 public class ProfileActivity extends AppCompatActivity {
     SharedPreferences pageL;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        getSupportActionBar().setTitle("Профиль");                                                  //название страницы
         pageL = getSharedPreferences(Сonfiguration.CURRENT_PAGE, Context.MODE_PRIVATE);
-        customizationActionBar();
-        ImageButton registration= findViewById(R.id.imageButton2);
+        ImageButton registration = findViewById(R.id.imageButton2);
         registration.setOnClickListener(v -> {
             Intent intent = new Intent(ProfileActivity.this, PageReadActivity.class);
             startActivity(intent);
         });
 
     }
-    void customizationActionBar(){
-        getSupportActionBar().setTitle("Профиль");                                   //название страницы
-    }}
+}
+
