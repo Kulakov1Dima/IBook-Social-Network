725cf5eb13241bf47407a42be2cff8b74dcebe0b
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-27 02:58:50 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/IBook/MainActivity.java
+++ app/src/main/java/com/example/IBook/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
             startActivity(intent);
         });
+        TextView number = findViewById(R.id.phone);
+        String phone =  number.getText().toString();
     }
-    }
-
+}
