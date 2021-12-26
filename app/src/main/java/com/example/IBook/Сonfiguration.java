06df76e7debab03a81a290123bdcb17aee9fcf8d
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2021-12-27 02:58:39 +0300

    Update Сonfiguration.java

--- app/src/main/java/com/example/IBook/Сonfiguration.java
+++ app/src/main/java/com/example/IBook/Сonfiguration.java
@@ -3,9 +3,4 @@ package com.example.IBook;
 public class Сonfiguration {
 
     public static String CURRENT_PAGE = "currentPage";
-    public static String OLD_CURRENT_PAGE = "oldCurrentPage";
-    public static String numberOfPages = "numberOfPages";
-
-    public static String pdfDocument = "ukodeksrf.pdf";
-
 }
