0b9e3d5b196bf11498f1a2d00a9114ba731cc09f
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-01-03 06:45:42 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/IBook/MainActivity.java
+++ app/src/main/java/com/example/IBook/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.IBook;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -12,30 +11,31 @@ import androidx.appcompat.app.AppCompatActivity;
 Ibook - приложение соцсети.
 21.10.2021  01:09 Кулаков Дмитрий
  */
+
 public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        delActionBar();
+        delActionBar();                                                 //удаление загаловка
         nextActivity();
     }
+
     void delActionBar(){
         ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.hide();
     }
+
     String getPhone(){
         TextView number = findViewById(R.id.phone);
         return number.getText().toString();
     }
+
     void nextActivity(){
         ImageButton registration= findViewById(R.id.imageButton);
         registration.setOnClickListener(v -> {
             new SendingPost().execute("authorization",getPhone());
-            Intent intent = new Intent(MainActivity.this, PasswdActivity.class);
-            intent.putExtra("PHONE", getPhone());
-            startActivity(intent);
         });
     }
 }
