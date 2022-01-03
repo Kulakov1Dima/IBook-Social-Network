0055fa23be1dd48a23b3d89e9b35843af2a0494b
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-01-03 18:02:08 +0300

    add dialog window

--- app/src/main/java/com/example/IBook/MainActivity.java
+++ app/src/main/java/com/example/IBook/MainActivity.java
@@ -2,26 +2,24 @@ package com.example.IBook;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-/*
-Ibook - приложение соцсети.
-21.10.2021  01:09 Кулаков Дмитрий
+/**
+ * Ibook - приложение соцсети.
+ * 21.10.2021  01:09 Кулаков Дмитрий
  */
 
 public class MainActivity extends AppCompatActivity implements SendingPost.Callback {
-
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         delActionBar();                                                 //удаление загаловка
-        nextActivity();
+        serverPolling();                                                //опрос сервера во втором потоке
     }
 
     void delActionBar(){
@@ -35,15 +33,28 @@ public class MainActivity extends AppCompatActivity implements SendingPost.Callb
         return number.getText().toString();
     }
 
-    void nextActivity(){
-        ImageButton registration= findViewById(R.id.imageButton);
-        registration.setOnClickListener(v -> {
-            new SendingPost(this).execute("authorization",getPhone());
-            //Intent intent = new Intent(MainActivity.this, PasswdActivity.class);
-            //startActivity(intent);
-        });
+    void serverPolling(){
+        findViewById(R.id.imageButton).setOnClickListener(v -> new SendingPost(this).execute("authorization",getPhone()));
     }
-    public void callingBack(String s){
-        Log.e("server",s);
+
+    public void callingBack(String dataResponse){
+        if(String.valueOf(dataResponse).equals("{\"command\":\"authorization\",\"registration\":false}")){
+            dialogRegistration();
+        }
+    }
+
+    void dialogRegistration(){
+        AlertDialog.Builder registration = new AlertDialog.Builder(MainActivity.this);
+        registration.setMessage("Вы не зарегестрированны. Хотите зарегистрироваться?")
+                .setCancelable(false)
+                .setPositiveButton("Да", (dialog, which) -> {
+                    dialog.cancel();
+                    Intent intent = new Intent(MainActivity.this, PasswdActivity.class);
+                    startActivity(intent);
+                })
+                .setNegativeButton("Нет", (dialog, which) -> finish());
+        AlertDialog alert = registration.create();
+        alert.setTitle("Регистрация");
+        alert.show();
     }
 }
