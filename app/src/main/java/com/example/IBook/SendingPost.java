a2156cee208693a7ec6f2d113c8160d8f64880f2
interpreter-It <84613812+interpreter-It@users.noreply.github.com>
2022-01-03 06:45:33 +0300

    Update SendingPost.java

--- app/src/main/java/com/example/IBook/SendingPost.java
+++ app/src/main/java/com/example/IBook/SendingPost.java
@@ -1,7 +1,9 @@
 package com.example.IBook;
 
-
 import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -11,13 +13,13 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-
 class SendingPost  extends AsyncTask <String, Void, Void> {
+    String responseStr;
+
     @Override
     protected Void doInBackground(String... strings) {
         if(strings[0].equals("authorization")) {
             MediaType mediaType = MediaType.parse("application/json");
-            String url = "https://orj00w.deta.dev/";
             OkHttpClient httpClient = new OkHttpClient();
             String jsonStr = "{\n" +
                     "  \"request\": {\n" +
@@ -27,16 +29,32 @@ class SendingPost  extends AsyncTask <String, Void, Void> {
                     "  \"version\": \"1.0\"\n" +
                     "}";
             Request request = new Request.Builder()
-                    .url(url)
+                    .url(Configuration.url)
                     .post(RequestBody.create(mediaType, jsonStr))
                     .build();
             try {
                 Response response = httpClient.newCall(request).execute();
-                assert !response.isSuccessful() || response.body() != null;
+                assert response.body() != null;
+                responseStr= response.body().string();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return null;
     }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        try {
+            JSONObject responseObj = new JSONObject(responseStr);
+            JSONObject data= responseObj.getJSONObject("response");
+            if(String.valueOf(data).equals("{\"command\":\"authorization\",\"registration\":false}")) {
+                // как-то нужно перейти на другое активити
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
